5bf5520b751bf260d682c45ceec6d161aede08f7
Matheus Wisniewski <matheus.maw@gmail.com>
2016-05-01 16:04:24 -0300

    Add TREINAR_REDE_NEURAL function to programatically train neural network (no GUI).

---
+++ TREINAR_REDE_NEURAL.m
@@ -0,0 +1,29 @@
+
+function [ ] = TREINAR_REDE_NEURAL( )
+
+    addpath .\Functions
+    addpath .\Databases
+    
+    load BancoDeDados
+    
+    erroDoTreinamento = 1;
+    numeroDeTreinamentos = 0;
+    
+    while (erroDoTreinamento > 0.01 && numeroDeTreinamentos < 5)
+        net = patternnet(50);
+        net.trainParam.showWindow = false;
+        net = train(net,entradaRedeNeural',saidaRedeNeural');
+
+        resultados = net(entradaRedeNeural');
+        erroDoTreinamento = perform(net,saidaRedeNeural',resultados);
+        %classes = vec2ind(y);
+        
+        numeroDeTreinamentos = numeroDeTreinamentos + 1;
+    end
+    
+    erroDoTreinamento
+    numeroDeTreinamentos
+    save ./Databases/BDRedeNeural net;
+
+end
+
