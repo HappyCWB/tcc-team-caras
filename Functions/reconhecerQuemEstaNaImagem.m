e7c10cac8f01db5c4c74e1cb6bd9107545cbb115
Matheus Wisniewski <matheus.maw@gmail.com>
2016-05-07 19:55:09 -0300

    Separate recognition from printing results.

--- Functions/reconhecerQuemEstaNaImagem.m
+++ Functions/reconhecerQuemEstaNaImagem.m
@@ -1,34 +1,9 @@
-function reconhecerQuemEstaNaImagem( imagemCortada80por60gray, temRostoNaImagem )
-%UNTITLED Summary of this function goes here
-%   Detailed explanation goes here
-
-    addpath ../Databases
-    addpath ../Classes
+function [taxaDeCerteza, idDaPessoa] = reconhecerQuemEstaNaImagem( imagemCortada80por60gray )
 
     entradaDoTeste = reshape(imagemCortada80por60gray, 4800, 1);
     
     load BDRedeNeural net
-    load BancoDeDados cadastro
-    
-    [taxaDeCerteza, idDaPessoa] = max(net(entradaDoTeste))
 
-    if taxaDeCerteza < 0.6
-        
-        if temRostoNaImagem
-                
-            disp('Vejo um rosto, mas não sei quem é...');
-                
-        else
-            
-            disp('AMBIENTE...');
-
-        end
-        
-    else
-        
-    disp(['Olá, ' cadastro.nomeDoID(idDaPessoa) '.']);
-
-    end
-
+    [taxaDeCerteza, idDaPessoa] = max(net(entradaDoTeste));
 end
 
