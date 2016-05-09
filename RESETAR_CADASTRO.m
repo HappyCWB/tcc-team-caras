10d53fe02f8b6d8efd806219b909f39b4a28afe0
Matheus Wisniewski <matheus.maw@gmail.com>
2016-05-08 22:53:48 -0300

    Add check to remove warnings from Matlab.

--- RESETAR_CADASTRO.m
+++ RESETAR_CADASTRO.m
@@ -3,6 +3,10 @@ function RESETAR_CADASTRO
     clearvars
     
     save ./Databases/BancoDeDados
-
+    
+    tamanhoAtual = 0;
+    save ./Databases/BancoDeDados tamanhoAtual
+    
+    clearvars
 end
 
