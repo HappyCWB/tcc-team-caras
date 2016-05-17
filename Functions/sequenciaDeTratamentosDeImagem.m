0b26ad69298d6602ddd4622e00561945c035bed2
Matheus Wisniewski <matheus.maw@gmail.com>
2016-05-16 23:35:37 -0300

    Reduce number of Erosions and Dilations to increase efficiency.

--- Functions/sequenciaDeTratamentosDeImagem.m
+++ Functions/sequenciaDeTratamentosDeImagem.m
@@ -3,16 +3,13 @@ function [ imagemPosTratamentos ] = sequenciaDeTratamentosDeImagem( imagemPreTra
     imagemPosPreenchimento0 = preencherBuracosDaImagem(imagemPreTratamentos);
 
     [imagemPosPreenchimento1, imagemPosErosao1, imagemPosDilatacao1] = ...
-           removerRuidosComQuadradoVariavel(imagemPosPreenchimento0, 7);
+           removerRuidosComQuadradoVariavel(imagemPosPreenchimento0, 5);
 
     [imagemPosPreenchimento2, imagemPosErosao2, imagemPosDilatacao2] = ...
-           removerRuidosComQuadradoVariavel(imagemPosPreenchimento1, 9);
+           removerRuidosComQuadradoVariavel(imagemPosPreenchimento1, 7);
     
-    [imagemPosPreenchimento3, imagemPosErosao3, imagemPosDilatacao3] = ...
-           removerRuidosComQuadradoVariavel(imagemPosPreenchimento2, 11);
-
-    [imagemPosTratamentos, imagemPosErosao4, imagemPosDilatacao4] = ...
-           removerRuidosComQuadradoVariavel(imagemPosPreenchimento3, 13);
+    [imagemPosTratamentos, imagemPosErosao3, imagemPosDilatacao3] = ...
+           removerRuidosComQuadradoVariavel(imagemPosPreenchimento2, 9);
     
     if(mostrarResultadosIntermediarios==1)
 
@@ -20,57 +17,45 @@ function [ imagemPosTratamentos ] = sequenciaDeTratamentosDeImagem( imagemPreTra
 
         suptitle('RESULTADOS INTERMEDIÁRIOS')
 
-        subplot(5,3,2)
+        subplot(4,3,2)
         imshow(imagemPreTratamentos);
         title('Segmentação de pele')
 
-        subplot(5,3,4)
+        subplot(4,3,4)
         imshow(imagemPosErosao1)
         title('Erosão 1')
 
-        subplot(5,3,5)
+        subplot(4,3,5)
         imshow(imagemPosDilatacao1)
         title('Dilatação 1')
 
-        subplot(5,3,6)
+        subplot(4,3,6)
         imshow(imagemPosPreenchimento1)
         title('Preenchida 1')
 
-        subplot(5,3,7)
+        subplot(4,3,7)
         imshow(imagemPosErosao2)
         title('Erosão 2')
 
-        subplot(5,3,8)
+        subplot(4,3,8)
         imshow(imagemPosDilatacao2)
         title('Dilatação 2')
 
-        subplot(5,3,9)
+        subplot(4,3,9)
         imshow(imagemPosPreenchimento2)
         title('Preenchida 2')
         
-        subplot(5,3,10)
+        subplot(4,3,10)
         imshow(imagemPosErosao3)
         title('Erosão 3')
 
-        subplot(5,3,11)
+        subplot(4,3,11)
         imshow(imagemPosDilatacao3)
         title('Dilatação 3')
 
-        subplot(5,3,12)
-        imshow(imagemPosPreenchimento3)
-        title('Preenchida 3')
-        
-        subplot(5,3,13)
-        imshow(imagemPosErosao4)
-        title('Erosão 4')
-
-        subplot(5,3,14)
-        imshow(imagemPosDilatacao4)
-        title('Dilatação 4')
-
-        subplot(5,3,15)
+        subplot(4,3,12)
         imshow(imagemPosTratamentos)
-        title('Preenchida 4')
+        title('Preenchida 3')
 
     end
 
