4db5fcbd6ec1cb72fa6c2d243dad74fb357bd066
Matheus Wisniewski <matheus.maw@gmail.com>
2016-05-24 00:44:31 -0300

    Correct extra Database.mat glitch

--- CADASTRO.m
+++ CADASTRO.m
@@ -103,9 +103,9 @@ function [  ] = CADASTRO(USAR_VIOLA_JONES, MOSTRAR_RESULTADOS_INTERMEDIARIOS, MO
 
         if get(checkboxPessoaAnterior, 'Value') == 0
             
-            save BancoDeDados cadastro
+            save .Databases/BancoDeDados cadastro
             idDaPessoa = escolherPessoaDaFoto();
-            load BancoDeDados cadastro
+            load .DatabasesBancoDeDados cadastro
 
             idMaisRecente = idDaPessoa;
             marcarCheckbox = 1;
@@ -212,7 +212,7 @@ function [  ] = CADASTRO(USAR_VIOLA_JONES, MOSTRAR_RESULTADOS_INTERMEDIARIOS, MO
         
         if tamanhoAtual > 0
             disp('Treinando Rede Neural...');
-            TREINAR_REDE_NEURAL(MOSTRAR_RESULTADOS_FINAIS);
+            treinarRedeNeural(MOSTRAR_RESULTADOS_FINAIS);
         end
         
         disp(' ');
