1181d57d308858dc7e4bde39232b8c8f621bc886
Matheus Wisniewski <matheus.maw@gmail.com>
2016-06-08 22:58:34 -0300

    Show both initial and new images side by side

--- Test/TESTE_HSV.m
+++ Test/TESTE_HSV.m
@@ -14,6 +14,12 @@ function TESTE_HSV (USAR_WEBCAM_INTEGRADA)
        
     imgHSV = rgb2hsv(imgRGB);
 
+    subplot(1,2,1)
+    imshow(imgRGB)
+    title('Imagem em RGB')
+    
+    subplot(1,2,2)
     imshow(imgHSV)
+    title('Imagem convertida para HSV (mantendo o mapa em RGB)')
 
 end
